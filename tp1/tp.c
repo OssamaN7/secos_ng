05de898acad5ddbfdfe08c5aeaf3c4f42562e331
OssamaN7 <ossama.aitel@icloud.com>
2025-10-08 14:40:08 +0200

    TP1:Q6:OK

--- tp1/tp.c
+++ tp1/tp.c
@@ -37,5 +37,57 @@ void print_gdt_content(gdt_reg_t gdtr_ptr) {
 
 
 void tp() {
-	// TODO
+	 
+    // Q1 ?
+
+     // GDTR and GDT configured by GRUB
+    gdt_reg_t gdtr_ptr;
+    get_gdtr(gdtr_ptr);
+    debug("GDT addr:  0x%x ", (unsigned int) gdtr_ptr.addr);
+    debug("limit: %d\n", gdtr_ptr.limit);
+    
+    print_gdt_content(gdtr_ptr);
+        // Q5
+    seg_desc_t my_gdt[7];
+    my_gdt[0].raw = 0ULL;
+    my_gdt[1].limit_1 = 0xffff;   //:16;     /* bits 00-15 of the segment limit */
+    my_gdt[1].base_1 = 0x0000;    //:16;     /* bits 00-15 of the base address */
+    my_gdt[1].base_2 = 0x00;      //:8;      /* bits 16-23 of the base address */
+    my_gdt[1].type = 11;//Code,RX //:4;      /* segment type */
+    my_gdt[1].s = 1;              //:1;      /* descriptor type */
+    my_gdt[1].dpl = 0; //ring0    //:2;      /* descriptor privilege level */
+    my_gdt[1].p = 1;              //:1;      /* segment present flag */
+    my_gdt[1].limit_2 = 0xf;      //:4;      /* bits 16-19 of the segment limit */
+    my_gdt[1].avl = 1;            //:1;      /* available for fun and profit */
+    my_gdt[1].l = 0; //32bits     //:1;      /* longmode */
+    my_gdt[1].d = 1;              //:1;      /* default length, depend on seg type */
+    my_gdt[1].g = 1;              //:1;      /* granularity */
+    my_gdt[1].base_3 = 0x00;      //:8;      /* bits 24-31 of the base address */
+    my_gdt[2].limit_1 = 0xffff;   //:16;     /* bits 00-15 of the segment limit */
+    my_gdt[2].base_1 = 0x0000;    //:16;     /* bits 00-15 of the base address */
+    my_gdt[2].base_2 = 0x00;      //:8;      /* bits 16-23 of the base address */
+    my_gdt[2].type = 3; //data,RW //:4;      /* segment type */
+    my_gdt[2].s = 1;              //:1;      /* descriptor type */
+    my_gdt[2].dpl = 0; //ring0    //:2;      /* descriptor privilege level */
+    my_gdt[2].p = 1;              //:1;      /* segment present flag */
+    my_gdt[2].limit_2 = 0xf;      //:4;      /* bits 16-19 of the segment limit */
+    my_gdt[2].avl = 1;            //:1;      /* available for fun and profit */
+    my_gdt[2].l = 0; // 32 bits   //:1;      /* longmode */
+    my_gdt[2].d = 1;              //:1;      /* default length, depend on seg type */
+    my_gdt[2].g = 1;              //:1;      /* granularity */
+    my_gdt[2].base_3 = 0x00;      //:8;      /* bits 24-31 of the base address */
+    
+
+    gdt_reg_t my_gdtr;
+    my_gdtr.addr = (int)my_gdt;
+    my_gdtr.limit = sizeof(my_gdt) - 1;
+    set_gdtr(my_gdtr);
+
+
+    get_gdtr(my_gdtr);
+    debug("NEW_GDT addr:  0x%x ", (unsigned int) my_gdtr.addr);
+    debug("limit: %d\n", my_gdtr.limit);
+    print_gdt_content(my_gdtr);
+
+
 }
