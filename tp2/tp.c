2cc199cc54361b24431d5134f1b86b3bbd97777b
OssamaN7 <ossama.aitel@icloud.com>
2025-10-08 17:27:33 +0200

    TP2:OK

--- tp2/tp.c
+++ tp2/tp.c
@@ -1,17 +1,30 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
+#include <segmem.h>
+#include <intr.h>
 
-void bp_handler() {
-   // TODO
+void  bp_handler()
+{
+    asm volatile("pusha"); 
+    debug("Handled BP interruption!\n");
+    asm volatile("popa");  
+    asm volatile("leave");
+    asm volatile("iret");  
+ }
+
+void bp_trigger()
+{
+    debug("Triggering BP...\n");
+    asm volatile("int3");
 }
 
-void bp_trigger() {
-	// TODO
-}
-
-void tp() {
-	// TODO print idtr
-
-	// TODO call bp_trigger
-   //bp_trigger();
+void tp()
+{
+    idt_reg_t idtr;
+    get_idtr(idtr);
+    int_desc_t *tab_int_desc = idtr.desc;
+    
+    build_int_desc(tab_int_desc+3, gdt_krn_seg_sel(1), (offset_t)&bp_handler);
+    
+    bp_trigger();
 }
