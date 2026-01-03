ea44ecbef98866511b8001ef258e8157d0cd3fb9
Ossama AIT-EL MOUDDENE <151570392+OssamaN7@users.noreply.github.com>
2026-01-03 23:43:11 +0100

    Implement user1 and user2 processes for counter management
    This file defines two processes, user1 and user2, for managing a shared counter in memory. user1 increments the counter, while user2 reads and prints it using a syscall.

--- kernel/core/users.c
+++ kernel/core/users.c
@@ -1,1 +1,40 @@
 
+/**
+ * 		users.c
+ * File defining the code of the 2 processes used to
+ * reprent users/processes using the kernel.
+ *  
+*/
+
+#include <users.h>
+
+/**
+ * user1()
+ * Represents the main function of process 1.
+ * Increments a counter in shared space memory.
+*/
+void user1() {
+	uint32_t *cpt = (uint32_t *) ((long unsigned int) SHARED_INT_PROC1_ADDR);
+	while (1) {
+        (*cpt)++;
+        // Used to debug the counter:
+        if (*cpt % 0xfffffff == 0)
+            asm volatile("int $0x80\t\n"::"S"(1));
+    }
+}
+
+/**
+ * user2() 
+ * Represents the main function of process 2.
+ * Reads and prints (syscall) the counter in shared space memory.
+*/
+void user2() {
+    // counter print syscall
+    void sys_counter() {
+        asm volatile("int $0x80\t\n"::"S"(1));
+    }
+	//uint32_t *cpt = (uint32_t *) ((long unsigned int) SHARED_INT_PROC2_ADDR);
+	while (1) {
+		sys_counter();
+	}
+}
